c5ac4e0c3400504cb6aec173ab0088a84eceae65
Ole Holm Frandsen <froksen@gmail.com>
2020-10-31 08:11:47 +0100

    renamed userscript

---
+++ administration-exportGroupmembersToCsv/administration-exportGroupmembersToCsv.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         AULA - Administration - Eksporter gruppedata til CSV
+// @namespace    
+// @version      0.3
+// @description  
+// @author       You
+// @match        https://www.aula.dk/administration/*
+// @grant        none
+// @require     https://raw.githubusercontent.com/eligrey/FileSaver.js/master/src/FileSaver.js
+// @require http://code.jquery.com/jquery-latest.js
+// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
+
+//@require       https://github.com/froksen/aUserscripts/raw/main/administration-exportGroupmembersToCsv/administration-exportGroupmembersToCsv.js
+
+//@downloadURL   https://github.com/froksen/aUserscripts/raw/main/administration-exportGroupmembersToCsv/administration-exportGroupmembersToCsv.user.js
+//@updateURL     https://github.com/froksen/aUserscripts/raw/main/administration-exportGroupmembersToCsv/administration-exportGroupmembersToCsv.user.js
+
+
+// ==/UserScript==
