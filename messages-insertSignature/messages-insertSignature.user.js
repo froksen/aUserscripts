a8121c082ad0eef521ed317531f0fa7c18971aa6
Ole Holm Frandsen <froksen@gmail.com>
2020-10-31 07:19:17 +0100

    added userscript and src for insert signaturee

---
+++ messages-insertSignature/messages-insertSignature.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         AULA - Beskeder - Indsæt signatur
+// @namespace
+// @version      0.0.5
+// @description  Indsætter en simpel signatur som standard i beskeder.
+// @author       Ole Frandsen
+
+// @include      https://www.aula.dk/portal/*
+// @require http://code.jquery.com/jquery-latest.js
+// @require http://code.jquery.com/ui/1.12.1/jquery-ui.js
+// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
+ //@require       https://github.com/froksen/aUserscripts/raw/main//messages-insertSignature/messages-insertSignature.js
+
+ //@downloadURL   https://github.com/froksen/aUserscripts/raw/main/messages-insertSignature/messages-insertSignature.user.js
+ //@updateURL     https://github.com/froksen/aUserscripts/raw/main/messages-insertSignature/messages-insertSignature.user.js 
+// @resource http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css
+// @grant GM_setValue
+// @grant GM_getValue
+
+// ==/UserScript==
