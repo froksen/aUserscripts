8cdd83608a0c737f0204477b6faaadf6f1649f25
Ole Holm Frandsen <froksen@gmail.com>
2020-10-31 07:22:20 +0100

    changed to correct script

--- messages-noticeAboutExternalContact/messages-noticeAboutExternalContact.user.js
+++ messages-noticeAboutExternalContact/messages-noticeAboutExternalContact.user.js
@@ -1,20 +1,10 @@
 // ==UserScript==
-// @name         AULA - Beskeder - Indsæt signatur
+// @name         AULA - Gør opmærksom på eksterne personer i beskeder
 // @namespace
-// @version      0.0.5
-// @description  Indsætter en simpel signatur som standard i beskeder.
-// @author       Ole Frandsen
-
-// @include      https://www.aula.dk/portal/*
+// @version      0.5
+// @description  Gør opmærksom på, at der er eksterne kontakter i beskeder f.eks. forældre.
+// @author       Ole Frandsen (OF)
+// @match        https://www.aula.dk/portal/*
+// @grant        none
 // @require http://code.jquery.com/jquery-latest.js
-// @require http://code.jquery.com/ui/1.12.1/jquery-ui.js
 // @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
- //@require       https://github.com/froksen/aUserscripts/raw/main/messages-noticeAboutExternalContact/messages-noticeAboutExternalContact.js
-
- //@downloadURL   https://github.com/froksen/aUserscripts/raw/main/messages-noticeAboutExternalContact/messages-noticeAboutExternalContact.user.js
- //@updateURL     https://github.com/froksen/aUserscripts/raw/main/messages-noticeAboutExternalContact/messages-noticeAboutExternalContact.user.js 
-// @resource http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css
-// @grant GM_setValue
-// @grant GM_getValue
-
-// ==/UserScript==
