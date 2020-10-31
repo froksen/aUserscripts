39f249089f9fa8127fd7c4df4c53f55b5d8aa058
Ole Holm Frandsen <froksen@gmail.com>
2020-10-31 08:00:08 +0100

    Create administration-removeExistingUsersInGroups.user.js
    administration-removeExistingUsersInGroups

---
+++ administration-removeExistingUsersInGroups/administration-removeExistingUsersInGroups.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         AULA Administration - Fjern alle eksisterende brugere
+// @namespace
+// @version      0.11
+// @description  Tilføjer knappen "Fjern alle brugere" til bruger administrationen
+// @author       Ole Frandsen
+
+// @include      https://www.aula.dk/administration/*
+// @grant        none
+// @require http://code.jquery.com/jquery-latest.js
+// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
+
+//@require       https://raw.githubusercontent.com/froksen/aUserscripts/main/administration-removeExistingUsersInGroups/administration-removeExistingUsersInGroups.js
+
+//@downloadURL   https://github.com/froksen/aUserscripts/raw/main/administration-removeExistingUsersInGroups/administration-removeExistingUsersInGroups.user.js
+//@updateURL     https://github.com/froksen/aUserscripts/raw/main/administration-removeExistingUsersInGroups/administration-removeExistingUsersInGroups.user.js
+
+// ==/UserScript==
