ea65e5ea6e54e8a9a417c37d8ed1367773c30c0f
Ole Holm Frandsen <froksen@gmail.com>
2020-10-31 07:05:27 +0100

    Create calendar-exportToOutlook.user.js
    Added first version of usr-script

---
+++ calender-exportToOutlook/calendar-exportToOutlook.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Eksporter kalenderbegivenhed til ICS
+// @namespace
+// @version      0.4
+// @description  Eksporter en kalenderbegivenhed fra AULAS calender til en ICS/Outlook
+// @author       Ole Frandsen
+// @match        https://www.aula.dk/portal/*
+// @require      https://code.jquery.com/jquery-3.5.1.min.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @require      https://raw.githubusercontent.com/eligrey/FileSaver.js/master/src/FileSaver.js
+// @require      https://raw.githubusercontent.com/eligrey/Blob.js/master/Blob.js
+// @require      https://raw.githubusercontent.com/froksen/ics.js/master/ics.js
+
+//@require       https://raw.githubusercontent.com/froksen/aUserscripts/main/calender-exportToOutlook/calendar-exportToOutlook.js
+
+//@downloadURL   https://github.com/froksen/aUserscripts/raw/main/calender-exportToOutlook/calendar-exportToOutlook.user.js
+//@updateURL     https://github.com/froksen/aUserscripts/raw/main/calender-exportToOutlook/calendar-exportToOutlook.user.js
+
+// ==/UserScript==
