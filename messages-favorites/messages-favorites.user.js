8d8cda6e4654b32414fc72983cc1c5ac9d658247
Ole Holm Frandsen <froksen@gmail.com>
2020-12-10 19:32:29 +0100

    Renamed file due to spelling mistake

--- messages-favorites/messages-favorites.user.js
+++ messages-favorites/messages-favorites.user.js
@@ -16,4 +16,5 @@
  //@downloadURL   https://github.com/froksen/aUserscripts/raw/main/messages-favorites/messages-favorites.user.js
  //@updateURL     https://github.com/froksen/aUserscripts/raw/main/messages-favorites/messages-favorites.user.js
 
+
   // ==/UserScript==
