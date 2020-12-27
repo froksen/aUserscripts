60062486eba7f960cba0608769f25dccb3de452f
Ole Holm Frandsen <froksen@gmail.com>
2020-12-27 20:48:23 +0100

    Updated update URLS

--- webproever - testresultater - exportData/webproever - testresultater - exportData.user.js
+++ webproever - testresultater - exportData/webproever - testresultater - exportData.user.js
@@ -5,4 +5,10 @@
 // @author       Ole Frandsen
 // @match       https://booking.webproever.dk/Rapporter/laerer/EleverPaaProeve.aspx?ID=*
 // @require http://code.jquery.com/jquery-latest.js
+
+//@require       https://github.com/froksen/aUserscripts/raw/main/webproever%20-%20testresultater%20-%20exportData/webproever%20-%20testresultater%20-%20exportData.js
+
+//@downloadURL   https://github.com/froksen/aUserscripts/raw/main/webproever%20-%20testresultater%20-%20exportData/webproever%20-%20testresultater%20-%20exportData.user.js
+//@updateURL     https://github.com/froksen/aUserscripts/raw/main/webproever%20-%20testresultater%20-%20exportData/webproever%20-%20testresultater%20-%20exportData.user.js
+
 // ==/UserScript==
