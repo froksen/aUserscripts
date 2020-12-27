e57aed19d4ae8e75ae1c080ed0b0ddfd52774000
Ole Holm Frandsen <froksen@gmail.com>
2020-12-27 20:46:17 +0100

    Added new usrScript for Gyldendals Webprøver.

---
+++ webproever - testresultater - exportData/webproever - testresultater - exportData.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         Webproever - Testresultater - Eksporter resultater til csv
+// @version      0.1
+// @description   Webproever - Testresultater - Eksporter resultater til csv
+// @author       Ole Frandsen
+// @match       https://booking.webproever.dk/Rapporter/laerer/EleverPaaProeve.aspx?ID=*
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
