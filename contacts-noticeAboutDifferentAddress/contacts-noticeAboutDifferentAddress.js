fcb9f423b370e9b80fad9cbb79cd77696c8e5109
Ole Holm Frandsen <froksen@gmail.com>
2021-01-24 20:00:03 +0100

    Added new userscript to compare contacts addresses

---
+++ contacts-noticeAboutDifferentAddress/contacts-noticeAboutDifferentAddress.js
@@ -0,0 +1,61 @@
+var debugMode = true;
+
+(function() {
+    'use strict';
+
+    var hasRun = false;
+
+    //Venter på elementet bliver synligt, og når synligt afvikler funktionen
+    waitForKeyElements(".profile-header",highlightActivity);
+
+    function highlightActivity()
+    {
+        if(hasRun)
+        {
+            return;
+        }
+
+        hasRun = true;
+
+
+        setTimeout(
+            function(){
+                var contactsAddresses = new Array();
+
+                //Finds all user profiles
+                $(".aula-profile-container").find(".aula-contact-info-container").each(function( person_index ) {
+                
+                //In every profile, go through every info
+                $(this).find('div.col').each(function(info_index){
+
+                    //If line matches regexp (address) then save it to array
+                    var str = $(this).text().trim();
+                    var patt = new RegExp(".* \d*, \d* .*");
+                    
+                    if (str.match(/.* \d*, \d* .*/g)) {
+                        contactsAddresses.push($(this));
+                     }
+
+                })
+
+                })
+                //Runs the test, and find if any has differnt address
+                var hasDifferntAddress = false;
+                for (var i = 1; i < contactsAddresses.length; i++) {
+                    
+                    if(contactsAddresses[i].text() != contactsAddresses[i-1].text())
+                    {
+                        hasDifferntAddress = true;
+                    }
+                }
+
+                //Print out information to user. 
+                if(hasDifferntAddress)
+                {
+                    $('<span id="" style="background-color:yellow"><b>OBS:</b> En eller flere personer har forskellige adresser!</span><br>').insertAfter(".contact-info-title");
+
+                }
+              },2000);
+    }
+
+})();
---
+++ contacts-noticeAboutDifferentAddress/contacts-noticeAboutDifferentAddress.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         AULA - Kontakter - Forskellig adresser
+// @namespace
+// @version      0.1
+// @description  Gør opmærksom på hvis personer har forskellige adresser
+// @author       Ole Frandsen (OF)
+// @match        https://www.aula.dk/portal/*
+// @grant        none
+// @require http://code.jquery.com/jquery-latest.js
+// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
+
+ //@require       https://raw.githubusercontent.com/froksen/aUserscripts/main/contacts-noticeAboutDifferentAddress/contacts-noticeAboutDifferentAddress.js
+
+ //@downloadURL   https://raw.githubusercontent.com/froksen/aUserscripts/main/contacts-noticeAboutDifferentAddress/contacts-noticeAboutDifferentAddress.user.js
+ //@updateURL     https://raw.githubusercontent.com/froksen/aUserscripts/main/contacts-noticeAboutDifferentAddress/contacts-noticeAboutDifferentAddress.user.js
+
+ // ==/UserScript==
