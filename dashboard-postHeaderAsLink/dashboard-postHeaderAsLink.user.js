13427165db78734ca88969832d8336e7ce0ec93b
Ole Holm Frandsen <froksen@gmail.com>
2023-08-17 15:31:46 +0200

    Update dashboard-postHeaderAsLink.user.js
    Nyt versionsnr

--- dashboard-postHeaderAsLink/dashboard-postHeaderAsLink.user.js
+++ dashboard-postHeaderAsLink/dashboard-postHeaderAsLink.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AULA - Overblik - Opslagsoverskrift som link
 // @namespace
-// @version      0.3
+// @version      0.4
 // @description  Gør overskriften i et opslag klikbar. 
 // @author       Ole Frandsen (OF)
 // @match        https://www.aula.dk/portal/*
