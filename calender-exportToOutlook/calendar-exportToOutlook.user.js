002d3bd1bf566de124bc2e686806268f627e4ebc
Ole Holm Frandsen <froksen@gmail.com>
2023-09-20 20:35:04 +0200

    Tilføjet tilladelse til GM_xmlhttpRequest

--- calender-exportToOutlook/calendar-exportToOutlook.user.js
+++ calender-exportToOutlook/calendar-exportToOutlook.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AULA - Kalender - Eksporter kalenderbegivenhed til ICS
 // @namespace
-// @version      0.2
+// @version      0.3
 // @description  Eksporter en kalenderbegivenhed fra AULAS calender til en ICS/Outlook
 // @author       Ole Frandsen
 // @match        https://www.aula.dk/portal/*
@@ -10,6 +10,8 @@
 // @require      https://raw.githubusercontent.com/eligrey/FileSaver.js/master/src/FileSaver.js
 // @require      https://raw.githubusercontent.com/eligrey/Blob.js/master/Blob.js
 // @require      https://raw.githubusercontent.com/froksen/ics.js/master/ics.js
+// @grant        GM_xmlhttpRequest
+
 
 //@require       https://raw.githubusercontent.com/froksen/aUserscripts/main/calender-exportToOutlook/calendar-exportToOutlook.js
 
